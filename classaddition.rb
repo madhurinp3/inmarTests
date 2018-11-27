528f957568b56d4dca497ad864976e78d6df91cc
madhurinp3 <45336522+madhurinp3@users.noreply.github.com>
2018-11-27 12:22:13 -0500

    Update classaddition.rb

--- classaddition.rb
+++ classaddition.rb
@@ -1,11 +1,18 @@
 #4. Define a class that has two properties x and y. Add a method to the class that adds x & y. Write a routine that uses this class to add two numbers. Write some test cases to test the addition method in the class
 
 class Addition
-   def add()
-     x=1
-     y=2
-     z = x+y
-   puts "Addition of two numbers is #{z}"
+
+  @@x = 1
+  @@y = 2
+
+  def add()
+
+    @z = @@x + @@y
+
+   puts "Addition of two numbers is #{@z}"
 
 end
 end
+
+b = Addition.new
+b.add()
