de7289cfabd24590a766c336c6445fad6ac0bf8b
madhurinp3 <45336522+madhurinp3@users.noreply.github.com>
2018-11-27 10:18:40 -0500

    Update StringReverse.rb

--- StringReverse.rb
+++ StringReverse.rb
@@ -1,6 +1,12 @@
+# Write a program to reverse a string “abcdef” --> “fedcba”
+
 str = "abcdef"
+puts "Original String is : #{str}"
 reverse = ""
+puts "string length #{str.length}"
+
 str.length.times do |i|
- reverse.insert(i,str[-1-i].chr)
+
+  reverse.insert(i,str[-1-i].chr)
 end
-puts reverse
+puts "Reverse String is : #{reverse}"
